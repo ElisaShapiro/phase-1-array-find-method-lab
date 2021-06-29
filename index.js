b1ba22672bc7416eac0943126b9928c747a5fa5b
Elisa Shapiro <elisashapiro@gmail.com>
2021-06-29 19:51:28 -0400

    commit tree 214e71030208d8f1864c73927cd44c5fa92799d0

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(win) {
+    let winningYear = win.find((win) => win.result === "W");
+    if (winningYear) {
+        return winningYear.year;
+    } else {
+        return 
+    }
+}
+superbowlWin(record);
